c482a37253febc26b635cc12fdbcc57eae1412a2
D4rk-L1nk <51430861+D4rk-L1nk@users.noreply.github.com>
2019-06-06 23:29:11 +0800

    Add files via upload

---
+++ fb.tools.sh
@@ -0,0 +1,374 @@
+
+pt="\033[37;1m"   #putih
+cn="\033[36;1m"   #cyan
+pr="\033[35;1m"  #purple
+br="\033[34;1m"   #biru
+kn="\033[33;1m"   #kuning
+hj="\033[32;1m"    #hijau
+mr="\033[31;1m"   #merah
+
+clear
+echo " "
+sleep 1
+figlet "WELCOME" | lolcat
+figlet "  TO   " | lolcat
+figlet "Dark-Link"
+figlet " TOOLS" | lolcat
+sleep 2
+clear
+sleep 0.5
+echo """
+$mr  █████   ███  █████  █  ██ $pt     █    █ █    █ █  ██
+$mr   █   █ █   █  █   █ █ █    $pt    █    █ ██   █ █ █
+$mr   █   █ █████  ████  ██    ██$pt██ █    █ █ ██ █ ██
+$pt   █   █ █   █  █ █   █ █    $mr    █    █ █   ██ █ █
+$pt  █████  █   █  █  ██ █  ██  $mr    ████ █ █    █ █  ██ """
+echo " "
+echo $kn"   F  A  C  E  B  O  O  K     T  O  O  L  S"
+echo $br"=============================================="
+echo $br"#"$hj" PANEL PILIHAN:                             "$br"#"
+echo $br"#"$hj"  01. MBF                                   "$br"#"
+echo $br"#"$hj"  02. OSIF                                  "$br"#"
+echo $br"#"$hj"  03. Profile Guard Facebook                "$br"#"
+echo $br"#"$hj"  04. Auto Reaction Facebook                "$br"#"
+echo $br"#············································#"
+echo $br"#"$hj"  88. Install semua tools                   "$br"#"
+echo $br"#"$hj"  99. About me/Contact Centre               "$br"#"
+echo $br"#"$hj"  00. Keluar/exit                           "$br"#"
+echo $br"=============================================="
+echo $cn" ___["$mr"MOD"$cn"]=["$mr"RUZTAM-D'ROCK"$cn"]"
+echo $cn"|"
+read -p"|__[+]>>~ " pil
+
+if [ $pil = 01 ] || [ $pil = 1 ]
+then
+clear
+sleep 2
+figlet -f slant "PROCESING..." | lolcat
+sleep 2
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|█████████████████                                         |"
+echo $pt"|█████████████████                                         |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ----------------------------------------------------------"
+echo $pt"|████████████████████████                                  |"
+echo $pt"|████████████████████████                                  |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████                            |"
+echo $pt"|██████████████████████████████                            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 2
+clear
+sleep 2
+git clone https://github.com/D4rk-L1nk/MBF-DL.git
+clear
+cd MBF-DL
+python2 MBF-Dark-Link.py
+fi
+
+if [ $pil = 02 ] || [ $pil = 2 ]
+then
+clear
+sleep 2
+figlet -f slant "PROCESING..." | lolcat
+sleep 2
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|█████████████████                                         |"
+echo $pt"|█████████████████                                         |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ----------------------------------------------------------"
+echo $pt"|████████████████████████                                  |"
+echo $pt"|████████████████████████                                  |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████                            |"
+echo $pt"|██████████████████████████████                            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 2
+clear
+sleep 2
+git clone https://github.com/D4rk-L1nk/OSIF-DL.git
+clear
+cd Osif-DL
+python2 Osif-DL.py
+fi
+
+if [ $pil = 03 ] || [ $pil = 3 ]
+then
+clear
+sleep 2
+figlet -f slant "PROCESING..." | lolcat
+sleep 2
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|█████████████████                                         |"
+echo $pt"|█████████████████                                         |"
+ echo $pt" ----------------------------------------------------------"
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ----------------------------------------------------------"
+echo $pt"|████████████████████████                                  |"
+echo $pt"|████████████████████████                                  |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████                            |"
+echo $pt"|██████████████████████████████                            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 2
+clear
+sleep 2
+git clone https://github.com/D4rk-L1nk/PGF-DL.git
+clear
+cd PGF-DL
+php guard.php
+fi
+
+if [ $pil = 04 ] || [ $pil = 4 ]
+then
+clear
+figlet -f slant "PROCESING..." | lolcat
+sleep 2
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|█████████████████                                         |"
+echo $pt"|█████████████████                                         |"
+echo $pt" ----------------------------------------------------------"
+sleep 1
+clear
+echo $pt" ----------------------------------------------------------"
+echo $pt"|████████████████████████                                  |"
+echo $pt"|████████████████████████                                  |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████                            |"
+echo $pt"|██████████████████████████████                            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt"|██████████████████████████████████████████████            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt"|██████████████████████████████████████████████████████████|"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 2
+clear
+sleep 2
+git clone https://github.com/D4rk-L1nk/Auto-reaction-fb.git
+clear
+cd Auto-reaction-fb
+sh start
+fi
+
+if [ $pil = 05 ] || [ $pil = 5 ]
+then
+clear
+figlet -f slant "PROCESING..." | lolcat
+sleep 2
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|█████████████████                                         |"
+echo $pt"|█████████████████                                         |"
+echo $pt" ----------------------------------------------------------"
+sleep 1
+clear
+echo $pt" ----------------------------------------------------------"
+echo $pt"|████████████████████████                                  |"
+echo $pt"|████████████████████████                                  |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+echo $pt" ---------------------------------------------------------- "
+echo $pt"|██████████████████████████████                            |"
+echo $pt"|██████████████████████████████                            |"
+echo $pt" ---------------------------------------------------------- "
+echo $pt"   L  O  A  D  I  N  G  ..."
+sleep 1
+clear
+fi
+
+if [ $pil = 88 ]
+then
+clear
+sleep 2
+figlet "WAIT..." | lolcat
+sleep 2
+echo $hj"INSTALING ALL TOOLS..."
+sleep 2
+pkg update && pkg upgrade
+pkg install python2
+pkg install python
+pip2 install mecanize && pip2 install requests
+pip2 install --upgrade pip
+pkg install figlet && pkg install toilet && pkg install cowsay
+pip2 install urllib3 chardet certifi idna
+pkg install curl
+pip2 install -r requirements.txt
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git                                                                                                      pkg install php
+pkg install neofetch
+clear
+sleep 2
+figlet "D O N E " | lolcat
+sleep 2
+clear
+echo " "
+echo $hj"RE-ENTER COMMAND "$cn"["$pt"sh fb.tools.sh"$cn"]"$hj" to reload tools"
+echo " "
+sleep 3
+exit
+fi
+
+if [ $pil = 99 ]
+then
+clear
+sleep 2
+echo """
+
+ $mr       ^^
+       ----                       ^^
+       \   \                     ----    R
+        \   \                   /   /
+         \   \    ___   ___    /   /
+          \   \  |   | |   |  /   /      O
+           \   \ |   | |   | /   /                    R
+            \   \|   | |   |/   /
+            |______            /         C     -N-
+           /       |>>        /
+          /     ___|>>        |                       O
+          \    /             |           K
+           |    \            |
+            |    \          |                         L
+             |             |
+              |           |
+               |         |                            L
+                ---------
+"""
+echo $cn"     ======================================================="
+echo $hj"                      ABOUT ME INFORMATION"
+echo $cn"     ======================================================="
+sleep 0.5
+echo $cn"      |"$mr"  Author   : RUZTAM D'ROCK                         "$cn"|"
+sleep 0.5
+echo $cn"      |"$hj"  Facebook : https://m.facebook.com/dark.link.666  "$cn"|"
+sleep 0.5
+echo $cn"      |"$hj"                    (Ruztam D'RockZ)               "$cn"|"
+sleep 0.5
+echo $cn"      |"$hj"  Phone/WA : +62 823 9820 7776                     "$cn"|"
+sleep 0.5
+echo $cn"      |"$hj"  E-mail   : rustamlarustamsapal@gmail.com         "$cn"|"
+sleep 0.5
+echo $cn"      |"$hj"  GIT      : ACTIVE ON THE GITHUB                  "$cn"|"
+echo $cn"     ======================================================="
+sleep 1
+echo
+echo "   -----------------------------------------------------------"
+echo $kn"    JIKA SOBAT MENGALAMI KENDALA DALAM MENJALANKAN TOOLS INI,"
+echo "                    SILAHKAN HUBUNGI "$mr"AUTHOR"
+sleep 1
+echo $cn"   ###########################################################"
+sleep 2
+echo " "
+echo $hj"RE-ENTER COMMAND "$cn"["$pt"sh fb.tools.sh"$cn"]"$hj" to reload tools"
+echo " "
+sleep 2
+fi
+
+if [ $pil = 00 ]
+then
+clear
+sleep 1
+echo
+echo " "
+echo "          TERIMA KASIH TELAH MENGGUNAKAN TOOLS INI"
+sleep 1
+echo "  ##########################################################"
+sleep 1
+echo "             Sampai jumpa kembali di lain waktu"
+sleep 1
+echo "  ##########################################################"
+sleep 2
+clear
+figlet -f slant "E X I T I N G" | lolcat
+sleep 3
+clear
+exit
+fi
+
